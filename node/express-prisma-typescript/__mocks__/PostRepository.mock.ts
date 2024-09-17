d44510ee38a80dca714adda0a588976e8e56a2bc
mate <matemorinigo@gmail.com>
2024-09-17 12:14:44 -0300

    Fixes

--- node/express-prisma-typescript/__mocks__/PostRepository.mock.ts
+++ node/express-prisma-typescript/__mocks__/PostRepository.mock.ts
@@ -1,6 +1,7 @@
 const PostRepositoryMock = {
   create: jest.fn(),
   getAllByDatePaginated: jest.fn(),
+  getFollowingByDatePaginated: jest.fn(),
   delete: jest.fn(),
   getById: jest.fn(),
   getByAuthorId: jest.fn()
